5a301a28a708c6e6dd71c1b4bfb6642612a89778
Shi Lixiang <wangzheslx@gmail.com>
2025-02-09 13:15:47 +0800

    feat: Update IDL
    Signed-off-by: Shi Lixiang <wangzheslx@gmail.com>

---
+++ backend/application/cart/internal/data/data.go
@@ -0,0 +1,82 @@
+package data
+
+import (
+	"backend/application/cart/internal/conf"
+	"backend/application/cart/internal/data/models"
+	"context"
+	"fmt"
+
+	"github.com/casdoor/casdoor-go-sdk/casdoorsdk"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/redis/go-redis/v9"
+)
+
+// ProviderSet is data providers.
+var ProviderSet = wire.NewSet(NewData, NewDB, NewCache, NewCasdoor)
+
+type Data struct {
+	db  *models.Queries
+	rdb *redis.Client
+	cs  *casdoorsdk.Client
+}
+
+// NewData .
+func NewData(
+	pgx *pgxpool.Pool,
+	rdb *redis.Client,
+	cs *casdoorsdk.Client,
+	logger log.Logger,
+) (*Data, func(), error) {
+	cleanup := func() {
+		log.NewHelper(logger).Info("closing the data resources")
+	}
+	return &Data{
+		db:  models.New(pgx),
+		rdb: rdb,
+		cs:  cs,
+	}, cleanup, nil
+}
+
+func NewDB(c *conf.Data) *pgxpool.Pool {
+	fmt.Printf("connecting to the database: %s\n", c.Database)
+	conn, err := pgxpool.New(context.Background(), c.Database.Source)
+	if err != nil {
+		panic(fmt.Sprintf("Unable to connect to database: %v", err))
+	}
+
+	return conn
+}
+
+func NewCache(c *conf.Data) *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Protocol: 3,
+		Addr:     c.Cache.Addr,
+		// Username:     c.Cache.Username,
+		// Password:     c.Cache.Password,
+		DialTimeout:  c.Cache.DialTimeout.AsDuration(),
+		ReadTimeout:  c.Cache.ReadTimeout.AsDuration(),
+		WriteTimeout: c.Cache.WriteTimeout.AsDuration(),
+	})
+
+	return rdb
+}
+
+func NewCasdoor(cc *conf.Auth) *casdoorsdk.Client {
+	client := casdoorsdk.NewClient(
+		cc.Casdoor.Server.Endpoint,
+		cc.Casdoor.Server.ClientId,
+		cc.Casdoor.Server.ClientSecret,
+		cc.Jwt.Certificate,
+		cc.Casdoor.Server.Organization,
+		cc.Casdoor.Server.Application,
+	)
+
+	return client
+}
+
+type userRepo struct {
+	data *Data
+	log  *log.Helper
+}
