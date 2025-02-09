b5793be5195ced71d16a360305cb89873276817e
Sumery <rcc@vip.qq.com>
2025-02-09 16:28:11 +0800

    fix: fix error

--- backend/application/product/internal/service/productcatalogservice.go
+++ backend/application/product/internal/service/productcatalogservice.go
@@ -12,17 +12,97 @@ type ProductCatalogServiceService struct {
 	pc *biz.ProductUsecase
 }
 
-func NewProductCatalogServiceService(pc *biz.ProductUsecase) *ProductCatalogServiceService {
-	return &ProductCatalogServiceService{pc: pc}
+func (s *ProductCatalogServiceService) UpdateProduct(ctx context.Context, product *pb.Product) (*pb.ProductReply, error) {
+	// TODO implement me
+	panic("implement me")
+}
+
+func (s *ProductCatalogServiceService) SearchProducts(ctx context.Context, req *pb.SearchProductsReq) (*pb.SearchProductsResp, error) {
+	products, err := s.ps.SearchProducts(ctx, &biz.SearchProductsReq{Query: req.GetQuery()})
+	if err != nil {
+		return nil, err
+	}
+	pbProduct := make([]*pb.Product, len(products.Result))
+	for i, product := range products.Result {
+		pbProduct[i] = &pb.Product{
+			Id:          product.Id,
+			Name:        product.Name,
+			Description: product.Description,
+			Picture:     product.Picture,
+			Price:       product.Price,
+			Categories:  product.Categories,
+		}
+	}
+	return &pb.SearchProductsResp{
+		Results: pbProduct,
+	}, nil
+}
+
+func (s *ProductCatalogServiceService) CreateProduct(ctx context.Context, req *pb.CreateProductRequest) (*pb.ProductReply, error) {
+	p, err := s.ps.CreateProduct(ctx, &biz.CreateProductRequest{
+		Owner:       req.Owner,
+		Username:    req.Username,
+		Name:        req.Name,
+		Description: req.Description,
+		Picture:     req.Picture,
+		Price:       req.Price,
+		Categories:  req.Categories,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &pb.ProductReply{
+		Product: &pb.Product{
+			Id:          p.Product.Id,
+			Name:        p.Product.Name,
+			Description: p.Product.Description,
+			Picture:     p.Product.Picture,
+			Price:       p.Product.Price,
+			Categories:  p.Product.Categories,
+		},
+	}, nil
 }
 
 func (s *ProductCatalogServiceService) ListProducts(ctx context.Context, req *pb.ListProductsReq) (*pb.ListProductsResp, error) {
+	list, err := s.ps.ListProducts(ctx, &biz.ListProductsReq{
+		Page:         uint(req.Page),
+		PageSize:     uint(req.PageSize),
+		CategoryName: req.CategoryName,
+	})
+	if err != nil {
+		return nil, err
+	}
+	pbProduct := make([]*pb.Product, len(list.Product))
+	for i, product := range list.Product {
+		pbProduct[i] = &pb.Product{
+			Id:          product.Id,
+			Name:        product.Name,
+			Description: product.Description,
+			Picture:     product.Picture,
+			Price:       product.Price,
+			Categories:  product.Categories,
+		}
+	}
+	return &pb.ListProductsResp{
+		Products: pbProduct,
+	}, nil
+}
 
-	return &pb.ListProductsResp{}, nil
+func (s *ProductCatalogServiceService) GetProduct(ctx context.Context, req *pb.GetProductReq) (*pb.ProductReply, error) {
+	product, err := s.ps.GetProduct(ctx, req.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.ProductReply{Product: &pb.Product{
+		Id:          product.Product.Id,
+		Name:        product.Product.Name,
+		Description: product.Product.Description,
+		Picture:     product.Product.Picture,
+		Price:       product.Product.Price,
+		Categories:  product.Product.Categories,
+	}}, nil
 }
-func (s *ProductCatalogServiceService) GetProduct(ctx context.Context, req *pb.GetProductReq) (*pb.GetProductResp, error) {
-	return &pb.GetProductResp{}, nil
-}
-func (s *ProductCatalogServiceService) SearchProducts(ctx context.Context, req *pb.SearchProductsReq) (*pb.SearchProductsResp, error) {
-	return &pb.SearchProductsResp{}, nil
+
+func NewServiceProductCatalogServiceService(ps *biz.ProductUsecase) *ProductCatalogServiceService {
+	return &ProductCatalogServiceService{ps: ps}
 }
