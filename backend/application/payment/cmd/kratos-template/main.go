e19e2fc98df3a5ff06d36b8c36e2c152a5216bc3
suyiiyii <suyiiyii@gmail.com>
2025-02-20 17:35:47 +0800

    怎么跑不起来

--- backend/application/payment/cmd/kratos-template/main.go
+++ backend/application/payment/cmd/kratos-template/main.go
@@ -58,6 +58,7 @@ func newApp(logger log.Logger, gs *grpc.Server,
 }
 
 func main() {
+	flag.Parse()
 	logger := log.With(log.NewStdLogger(os.Stdout),
 		"ts", log.DefaultTimestamp,
 		"caller", log.DefaultCaller,
