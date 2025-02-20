670914e60a2ded70ee4c971486be73b847a5c883
suyiiyii <suyiiyii@gmail.com>
2025-02-20 14:50:12 +0800

    try to add DI code

--- backend/application/payment/internal/service/paymentservice.go
+++ backend/application/payment/internal/service/paymentservice.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	"backend/application/payment/internal/biz"
 	"context"
 
 	pb "backend/api/payment/v1"
@@ -8,10 +9,13 @@ import (
 
 type PaymentServiceService struct {
 	pb.UnimplementedPaymentServiceServer
+	oc *biz.UserUsecase
 }
 
-func NewPaymentServiceService() *PaymentServiceService {
-	return &PaymentServiceService{}
+func NewPaymentServiceService(oc *biz.UserUsecase) *PaymentServiceService {
+	return &PaymentServiceService{
+		oc: oc,
+	}
 }
 
 func (s *PaymentServiceService) Charge(ctx context.Context, req *pb.ChargeReq) (*pb.ChargeResp, error) {
